efb2230e5aaee7a649980cc80d4ab8e63c5f667e
Long - Thinkpad <ntl.284@gmail.com>
2020-11-08 16:23:51 +0700

    Update SinhVienService.js

--- In Class/Buoi15_quanlysinhvien_table_js-20201031/services/SinhVienService.js
+++ In Class/Buoi15_quanlysinhvien_table_js-20201031/services/SinhVienService.js
@@ -11,11 +11,10 @@ var SinhVienService = function () {
 
   this.themSinhVien = function (sv) {
     var promise = axios({
-        url: "http://svcy.myclass.vn/api/SinhVienApi/ThemSinhVien", //Api backend cung cấp
-        method: "POST", // giao thức backend cung cấp
-        data: sv //Dữ liệu gửi đi (lưu ý: dữ liệu gửi đi phải format dữ liệu của Backend yêu cầu)
-  });
-  return promise;
-}
-
-}
+      url: "http://svcy.myclass.vn/api/SinhVienApi/ThemSinhVien", //Api backend cung cấp
+      method: "POST", // giao thức backend cung cấp
+      data: sv, //Dữ liệu gửi đi (lưu ý: dữ liệu gửi đi phải format dữ liệu của Backend yêu cầu)
+    });
+    return promise;
+  };
+};
